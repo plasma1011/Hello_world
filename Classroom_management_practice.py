6400099818e694f2b484e33101574addea5729c3
plasma1011 <jm558913@georgetownisd.org>
2026-01-16 13:32:13 -0600

    Add files via upload
    i.e. My first commit to GitHub

---
+++ Classroom_management_practice.py
@@ -0,0 +1,26 @@
+firstperiod = ["Tony","Gina","jake","scrully"]#this is firstperiod
+thirdperiod = ["Amy","Santi","Charles","rosa"]#this is thirdperiod
+fourthperiod = ["will","George","Nancy","shawn"]#this is fourthperiod
+topstudents = []
+topstudents.append(firstperiod[1])
+topstudents.append(thirdperiod[2])
+topstudents.append(fourthperiod[0])
+topscores = [93.5,98,96]#these are the topscores and integers
+averagescores = (topscores[0]+topscores[1]+topscores[2])/3
+print(topstudents) 
+print(firstperiod)
+print(thirdperiod)
+print(fourthperiod)
+print(topscores)
+ginascore = [topstudents[0],topscores[0]]
+print(ginascore)
+print(ginascore[0],"scored",ginascore[1])
+Charlesscore = [topstudents[1],topscores[1]]
+print(Charlesscore)
+print(Charlesscore[0],"scored",Charlesscore[1])
+willscore = [topstudents[2],topscores[2]]
+print(willscore)
+print(willscore[0],"scored",willscore[1])
+print(averagescores)
+print("the average top score across all classes is")
+print(averagescores)
